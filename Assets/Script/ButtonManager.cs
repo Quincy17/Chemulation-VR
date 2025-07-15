9b9a49b7f721e14320fa0498bcc768777a040a70
FarrelCaesarian <farrelcaesarian15@gmail.com>
2025-07-15 21:35:41 +0700

    add code for main menu canvas (Start, Setting, Exit)

---
+++ Assets/Script/ButtonManager.cs
@@ -0,0 +1,242 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    // public MatchTheOrganShuffler organShuffler;
+    // public MatchTheOrganGameManager organGameManager;
+
+    public UIManager uiManager;
+    // public HomeUIManager homeUIManager;
+
+    public Transform humanOrgansParent;
+    public GameObject findTheOrganGameManager;
+
+    private Dictionary<Transform, Vector3> initialPositions = new Dictionary<Transform, Vector3>();
+    private Dictionary<Transform, Quaternion> initialRotations = new Dictionary<Transform, Quaternion>();
+
+    private int clickCount = 0;
+    private Coroutine rotateCoroutine;
+
+    // public OrganHider organHider;
+
+    // void Awake()
+    // {
+    //     // Simpan posisi & rotasi awal organ secara rekursif
+    //     SaveInitialTransformsRecursive(humanOrgansParent);
+    // }
+
+    // // Fungsi untuk mengulang posisi semua organ
+    // public void ResetAllOrgans()
+    // {
+    //     foreach (var entry in initialPositions)
+    //     {
+    //         Transform organ = entry.Key;
+    //         organ.localPosition = entry.Value;
+    //         organ.localRotation = initialRotations[organ];
+
+    //         if (!organ.gameObject.activeSelf)
+    //             organ.gameObject.SetActive(true);
+    //     }
+    // }
+
+    // // Fungsi untuk menyembunyikan bagian tubuh bertahap
+    // public void HideButton()
+    // {
+    //     clickCount++;
+
+    //     switch (clickCount)
+    //     {
+    //         case 1:
+    //             SetBodyPartActive("Body-Front", false);
+    //             break;
+    //         case 2:
+    //             SetBodyPartActive("Body-Back", false);
+    //             break;
+    //         case 3:
+    //             ResetAndUnhideTagged("BodyFront");
+    //             ResetAndUnhideTagged("BodyBack");
+    //             clickCount = 0;
+    //             break;
+    //     }
+    // }
+
+    // private void ResetAndUnhideTagged(string tag)
+    // {
+    //     foreach (var entry in initialPositions)
+    //     {
+    //         Transform t = entry.Key;
+    //         if (t.CompareTag(tag))
+    //         {
+    //             t.localPosition = initialPositions[t];
+    //             t.localRotation = initialRotations[t];
+    //             t.gameObject.SetActive(true);
+    //         }
+    //     }
+    // }
+
+    // private void SetBodyPartActive(string name, bool isActive)
+    // {
+    //     SetActiveRecursive(humanOrgansParent, name, isActive);
+    // }
+
+    // private void SetActiveRecursive(Transform parent, string name, bool isActive)
+    // {
+    //     foreach (Transform t in parent)
+    //     {
+    //         if (t.name == name)
+    //         {
+    //             t.gameObject.SetActive(isActive);
+    //             return;
+    //         }
+
+    //         if (t.childCount > 0)
+    //             SetActiveRecursive(t, name, isActive);
+    //     }
+    // }
+
+    // private void SaveInitialTransformsRecursive(Transform parent)
+    // {
+    //     foreach (Transform child in parent)
+    //     {
+    //         initialPositions[child] = child.localPosition;
+    //         initialRotations[child] = child.localRotation;
+
+    //         if (child.childCount > 0)
+    //             SaveInitialTransformsRecursive(child);
+    //     }
+    // }
+
+    // Mode: Find The Organ
+    // public void StartFindTheOrganGame()
+    // {
+    //     ResetAllOrgans();
+
+    //     if (findTheOrganGameManager != null)
+    //     {
+    //         findTheOrganGameManager.GetComponent<FindTheOrganGameManager>().RestartGame();
+    //     }
+    //     else
+    //     {
+    //         Debug.LogWarning("FindTheOrganGameManager belum di-assign!");
+    //     }
+    // }
+
+    // // 🇬🇧 Set bahasa Inggris + mulai ulang
+    // public void SetLanguageToEnglish()
+    // {
+    //     var organGame = findTheOrganGameManager.GetComponent<FindTheOrganGameManager>();
+    //     if (organGame != null)
+    //     {
+    //         organGame.SetLanguage("english");
+    //         StartFindTheOrganGame();
+    //     }
+    // }
+
+    // // 🇮🇩 Set bahasa Indonesia + mulai ulang
+    // public void SetLanguageToIndonesian()
+    // {
+    //     var organGame = findTheOrganGameManager.GetComponent<FindTheOrganGameManager>();
+    //     if (organGame != null)
+    //     {
+    //         organGame.SetLanguage("indonesian");
+    //         StartFindTheOrganGame();
+    //     }
+    // }
+
+    // // Mode: Match The Organ
+    // public void StartOrganPuzzle()
+    // {
+    //     organShuffler.ShuffleOrgans();
+    //     organGameManager.StartMatchOrganGame();
+    // }
+
+    // public void FinishOrganPuzzle()
+    // {
+    //     organGameManager.OnFinish();
+    // }
+
+    // public void CancelOrganPuzzle()
+    // {
+    //     organGameManager.OnCancel();
+    //     organShuffler.ResetAllOrgans();
+    // }
+
+    // UI Manager
+    public void OnPlayPressed() => uiManager.OnPlayPressed();
+    public void OnExitPressed() => uiManager.OnExitPressed();
+    public void OnClosePlayPressed() => uiManager.OnClosePlayPressed();
+    public void OnCloseExitPressed() => uiManager.OnCloseExitPressed();
+    public void OnExitYesPressed() => uiManager.OnExitYesPressed();
+
+    public void OnNextSlide() => uiManager.OnNextSlide();
+    public void OnPreviousSlide() => uiManager.OnPreviousSlide();
+
+    // UI Resume
+    // public void OnHomeButtonPressed() => homeUIManager.OnHomeButtonPressed();
+    // public void OnResumePressed() => homeUIManager.OnResumePressed();
+    // public void OnSelectLevelPressed() => homeUIManager.OnSelectLevelPressed();
+    // public void OnCloseSelectLevelPressed() => homeUIManager.OnCloseSelectLevelPressed();
+    // public void TeleportToHome()
+    // {
+    //     homeUIManager.TeleportToHome();
+    //     ResetAllOrgans(); // common
+
+    //     OrganExplorerUIManager.Instance?.CloseUI(); // tutup explorer UI
+
+    //     var fg = findTheOrganGameManager.GetComponent<FindTheOrganGameManager>();
+    //     fg?.ForceStopAndReset();
+
+
+    //     organHider.UnhideAll(); // Find the Organ
+    // }
+
+
+    // // Rotating the Organ Parents
+    // public void StartRotateParent(Transform targetParent, Vector3 axis, float speed = 50f)
+    // {
+    //     if (rotateCoroutine != null) StopCoroutine(rotateCoroutine);
+    //     rotateCoroutine = StartCoroutine(RotateContinuously(targetParent, axis, speed));
+    // }
+
+    // public void StopRotateParent()
+    // {
+    //     if (rotateCoroutine != null) StopCoroutine(rotateCoroutine);
+    //     rotateCoroutine = null;
+    // }
+
+    // private IEnumerator RotateContinuously(Transform target, Vector3 axis, float speed)
+    // {
+    //     while (true)
+    //     {
+    //         target.Rotate(axis * speed * Time.deltaTime, Space.World);
+    //         yield return null;
+    //     }
+    // }
+
+    // public void OnRotateButtonPressed()
+    // {
+    //     StartRotateParent(humanOrgansParent, Vector3.up, 50f); // rotate Y axis
+    // }
+
+    // public void OnStopRotatePressed()
+    // {
+    //     StopRotateParent();
+    // }
+
+    // // Hider in the Find the Organ
+
+    // public void OnHideButtonPressed()
+    // {
+    //     organHider.HideNext();
+    // }
+
+    // public void OnUnhideAllButtonPressed()
+    // {
+    //     ResetAllOrgans();
+
+    //     organHider.UnhideAll();
+    // }
+
+}
---
+++ Assets/Script/UIManager.cs
@@ -0,0 +1,163 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [Header("Canvas References")]
+    public GameObject mainMenuCanvas;
+    public GameObject playCanvas;
+    public GameObject exitCanvas;
+
+    [Header("Play Mode Panels & Images")]
+    public GameObject[] modePanels;         // [Panel]_Mode_1, [Panel]_Mode_2
+    public Image[] modeImages;              // Image inside the panels
+
+    [Header("Play Mode Navigation Buttons")]
+    public GameObject nextButton;
+    public GameObject prevButton;
+    public GameObject closeButton;
+
+    [Header("Camera Rig Control")]
+    public Transform rigTransform; // assign XR Rig / OVRCameraRig di Inspector
+
+    [Header("Sprites for Modes")]
+    public List<Sprite> modeSprites;        // Assign 4 sprites (for each mode)
+
+    [System.Serializable]
+    public class ModeData
+    {
+        public Vector3 position;
+        public float yRotation;
+        public bool isInteractable;
+    }
+
+    public List<ModeData> modeDataList = new List<ModeData>(); // 4 entries
+
+    private int currentPage = 0;
+    private const int modesPerPage = 2;
+
+    // Called by ButtonManager
+    public void OnPlayPressed()
+    {
+        StartCoroutine(SwitchCanvas(mainMenuCanvas, playCanvas));
+        currentPage = 0;
+        UpdatePlayModeSlide();
+    }
+
+    public void OnExitPressed()
+    {
+        StartCoroutine(SwitchCanvas(mainMenuCanvas, exitCanvas));
+    }
+
+    public void OnCloseExitPressed()
+    {
+        StartCoroutine(SwitchCanvas(exitCanvas, mainMenuCanvas));
+    }
+
+    public void OnClosePlayPressed()
+    {
+        StartCoroutine(SwitchCanvas(playCanvas, mainMenuCanvas));
+    }
+
+    public void OnExitYesPressed()
+    {
+        Application.Quit();
+    }
+
+    public void OnNextSlide()
+    {
+        currentPage++;
+        UpdatePlayModeSlide();
+    }
+
+    public void OnPreviousSlide()
+    {
+        currentPage--;
+        UpdatePlayModeSlide();
+    }
+
+    private void UpdatePlayModeSlide()
+    {
+        int start = currentPage * modesPerPage;
+
+        for (int i = 0; i < modesPerPage; i++)
+        {
+            int modeIndex = start + i;
+
+            if (modeIndex < modeSprites.Count && modeIndex < modeDataList.Count)
+            {
+                modePanels[i].SetActive(true);
+                modeImages[i].sprite = modeSprites[modeIndex];
+
+                // Assign panelSlotIndex dan uiManager ke ModePanelPoke
+                // ModePanelPoke poke = modePanels[i].GetComponent<ModePanelPoke>();
+                // if (poke != null)
+                // {
+                //     poke.panelSlotIndex = i;  // slot panel dalam slide (0 atau 1)
+                //     poke.uiManager = this;
+                // }
+
+                // Button logic tetap seperti biasa
+                Button btn = modePanels[i].GetComponent<Button>();
+                if (btn != null)
+                {
+                    btn.onClick.RemoveAllListeners();
+
+                    if (modeDataList[modeIndex].isInteractable)
+                    {
+                        btn.interactable = true;
+                        int capturedIndex = modeIndex;
+                        btn.onClick.AddListener(() => SelectMode(capturedIndex));
+                    }
+                    else
+                    {
+                        btn.interactable = false;
+                    }
+                }
+            }
+            else
+            {
+                modePanels[i].SetActive(false);
+            }
+        }
+
+        prevButton.SetActive(currentPage > 0);
+        nextButton.SetActive((currentPage + 1) * modesPerPage < modeSprites.Count && (currentPage + 1) * modesPerPage < modeDataList.Count);
+    }
+
+    // Fungsi lama yang masih dipakai button biasa
+    public void SelectMode(int index)
+    {
+        if (index >= 0 && index < modeDataList.Count && modeDataList[index].isInteractable)
+        {
+            rigTransform.position = modeDataList[index].position;
+            rigTransform.eulerAngles = new Vector3(0, modeDataList[index].yRotation, 0);
+        }
+    }
+
+    // Fungsi baru untuk manual select mode dari panel slot dan page saat ini
+    public void ManualSelectMode(int panelSlotIndex)
+    {
+        int modeIndex = currentPage * modesPerPage + panelSlotIndex;
+
+        Debug.Log($"ManualSelectMode dipanggil: currentPage={currentPage}, panelSlotIndex={panelSlotIndex}, modeIndex={modeIndex}");
+
+        if (modeIndex >= 0 && modeIndex < modeDataList.Count && modeDataList[modeIndex].isInteractable)
+        {
+            rigTransform.position = modeDataList[modeIndex].position;
+            rigTransform.eulerAngles = new Vector3(0, modeDataList[modeIndex].yRotation, 0);
+        }
+        else
+        {
+            Debug.LogWarning($"ManualSelectMode index {modeIndex} invalid atau tidak interactable");
+        }
+    }
+
+    private System.Collections.IEnumerator SwitchCanvas(GameObject currentCanvas, GameObject nextCanvas)
+    {
+        yield return new WaitForSeconds(0.1f);
+        currentCanvas.SetActive(false);
+        nextCanvas.SetActive(true);
+    }
+}
